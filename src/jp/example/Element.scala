b982d202152c634d10efca3bc73bcbcb09658227
nastizm <nastizm@gmail.com>
2011-04-05 01:49:19 +0900

    shapter 10 start

---
+++ src/jp/example/Element.scala
@@ -0,0 +1,11 @@
+package jp.example
+
+abstract class Element {
+	def contents: Array[String]
+	def height: Int = contents.length
+	def width: Int = if (height == 0) 0 else contents(0).length
+}
+
+class ArrayElement(conts: Array[String]) extends Element {
+	def contents: Array[String] = conts
+}
