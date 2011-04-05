ee67e1502544da224d7751a0e2570172bbeacdf3
nastizm <nastizm@gmail.com>
2011-04-06 01:47:09 +0900

    chapter 10 complete

--- src/jp/example/Element.scala
+++ src/jp/example/Element.scala
@@ -1,11 +1,67 @@
 package jp.example
 
+import Element._
+
+// chapter 10
 abstract class Element {
 	def contents: Array[String]
 	def height: Int = contents.length
 	def width: Int = if (height == 0) 0 else contents(0).length
+	def above(that: Element): Element = {
+		val this1 = this widen that.width
+		val that1 = that widen this.width
+		elem(this1.contents ++ that1.contents)
+	}
+	def beside(that: Element) = new ArrayElement(
+			for (
+			  (line1, line2) <- this.contents zip that.contents
+			) yield line1 + line2
+	)
+//	def beside(that: Element): Element = {
+//		val contents = new Array[String](this.contents.length)
+//		for (i <- 0 until this.contents.length)
+//			contents(i) = this.contents(i) + that.contents(i)
+//	  new ArrayElement(contents)
+//	}
+	def widen(w: Int): Element =
+		if (w <= width) this
+		else {
+			val left = elem(' ', (w - width) / 2, height)
+			val right = elem(' ', w - width - left.width, height)
+			left beside this beside right
+		}
+	def heighten(h: Int): Element =
+		if (h <= height) this
+		else {
+			val top = elem(' ', width, (h - height) / 2)
+			val bot = elem(' ', width, h - height - top.height)
+			top above this above bot
+		}
+	override def toString = contents mkString "\n"
 }
 
-class ArrayElement(conts: Array[String]) extends Element {
-	def contents: Array[String] = conts
+// 10.12
+object Element {
+	private class ArrayElement(val contents: Array[String]) extends Element
+	private class LineElement(s: String) extends Element {
+		val contents = Array(s)
+		override def width = s.length
+		override def height = 1
+	}
+	// 10.9
+	// 抽象クラスのElementを別の形で定義したものを継承可能
+	private class UniformElement(
+	  ch: Char,
+	  override val width: Int,
+	  override val height: Int
+	) extends Element {
+		private val line = ch.toString * width
+		def contents = Array.make(height, line)
+	}
+	def elem(contents: Array[String]): Element =
+		new ArrayElement(contents)
+	def elem(chr: Char, width: Int, height: Int): Element =
+		new UniformElement(chr, width, height)
+	def elem(line: String): Element =
+		new LineElement(line)
 }
